29007ac429f14968c70fea098e7e939af92bc155
hendr1an <hendriansyahgyan@gmail.com>
2025-06-10 10:58:02 +0700

    Menambahkan contoh exception std::out_of_range pada akses array

---
+++ exceptionStandard.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <exception>
+#include <array>
+using namespace std;
+
+int main ()
+{
+    cout << "awal program" << endl;
+    try
+    {
+        array<int, 3> data = {10, 20, 30};
+        cout << data.at(4) << endl;
+    }
+}
