fc8c3a4be50572da1237ba961aa4a18fc16a8fe7
hendr1an <hendriansyahgyan@gmail.com>
2025-06-10 11:16:26 +0700

    Inisialisasi struktur dasar program C++

--- ManualException.cpp
+++ ManualException.cpp
@@ -3,21 +3,5 @@ using namespace std;
 
 int main()
 {
-    try
-    {
-        cout << "selamat belajar di prodi TI UMY" << endl;
-        throw("halo");
-        cout << "pernyataan tidak akan dieksekusi " << endl;
-    }
-
-     catch(int a)
-    { 
-        cout << "pengecualian akan dieksekusi" << endl;
-    }
-    catch(...)
-    {
-      cout << "default pengecualiaan dieksekusi" << endl;
-    }
-
-    return 0;
+    
 }
