ec3fbb2cf16b33e3e29575477dbe9f28b753a29b
hendr1an <hendriansyahgyan@gmail.com>
2025-06-10 10:08:15 +0700

    Menambahkan fitur penulisan dan pembacaan file teks menggunakan C++

--- iofile.cpp
+++ iofile.cpp
@@ -21,4 +21,25 @@ int main ()
         outfile << baris << endl;
     }
 
+    outfile.close();
+
+ifstream infile;
+
+infile.open("contohfile.txt");
+
+cout << endl
+     << ">= membuka dan membaca file " << endl;
+
+if (infile.is_open())
+{
+    while (getline(infile, baris))
+    {
+       cout << baris << '\n';
+    }
+
+    infile.close();
+}
+else
+  cout << "unable to open this file";
+  return 0;
 }
