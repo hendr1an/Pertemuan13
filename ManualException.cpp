95d58f4c20a27e8f3d67e67437407a5f27346c77
hendr1an <hendriansyahgyan@gmail.com>
2025-06-10 11:17:07 +0700

    Menambahkan contoh penanganan exception dengan blok catch default

--- ManualException.cpp
+++ ManualException.cpp
@@ -3,5 +3,21 @@ using namespace std;
 
 int main()
 {
-    
+    try
+    {
+        cout << "selamat belajar di prodi TI UMY" << endl;
+        throw("halo");
+        cout << "pernyataan tidak akan dieksekusi " << endl;
+    }
+
+     catch(int a)
+    { 
+        cout << "pengecualian akan dieksekusi" << endl;
+    }
+    catch(...)
+    {
+      cout << "default pengecualiaan dieksekusi" << endl;
+    }
+
+    return 0;
 }
