b16535b03ad0025827792d71d023f28a7a4abd97
hendr1an <hendriansyahgyan@gmail.com>
2025-06-10 10:32:25 +0700

    Menambahkan input nama file untuk penulisan file teks di C++

---
+++ iofileWithPath.cpp
@@ -0,0 +1,20 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+   string baris;
+   string namafile;
+
+    cout << "memasukan nama file : ";
+    cin >> namafile;
+
+    ofstream outfile; 
+
+    outfile.open(namafile + ". txt", ios::out);
+    cout << ">= Menulis File, \'q\' untuk keluar" << endl;
+
+ 
+}
